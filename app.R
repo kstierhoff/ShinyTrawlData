54cc4bdfe2d138c459bfd22c406df4d267d1a51d
kstierhoff <kstierhoff@ucsc.edu>
2018-10-15 22:50:33 -0700

    Minor changes to allow connection to local database. Added test database to the project.

--- app.R
+++ app.R
@@ -10,25 +10,54 @@
 # Shiny database interface
 # https://shiny.rstudio.com/articles/overview.html
 
+library(ggplot2)
+library(dplyr)
 library(shiny)
 library(DBI)
+library(odbc)
+library(plotly)
+library(here)
+library(DT)
+
+options(shiny.maxRequestSize = 30*1024^2)
 
 ui <- fluidPage(
-    numericInput("nrows", "Enter the number of rows to display:", 5),
-    tableOutput("tbl")
+    # App title ----
+    titlePanel("Explore Trawl Database"),
+    
+    # Sidebar layout with input and output definitions ----
+    sidebarLayout(
+        
+        # Sidebar panel for inputs ----
+        sidebarPanel(
+            
+            # Input: Select a file ----
+            fileInput("db", "Choose .accdb File",
+                      multiple = FALSE,
+                      accept = c(".accdb")),
+            
+            # Input: Select number of rows to display ----
+            numericInput("nrows", "Enter the number of rows to display:", 5)
+        ),
+        
+        # Main panel for displaying outputs ----
+        mainPanel(
+            
+            # Output: Data file ----
+            tableOutput("tbl")
+            
+        )
+        
+    )
 )
 
 server <- function(input, output, session) {
     output$tbl <- renderTable({
-        conn <- dbConnect(
-            drv = RMySQL::MySQL(),
-            dbname = "shinydemo",
-            host = "shiny-demo.csa7qlmguqrf.us-east-1.rds.amazonaws.com",
-            username = "guest",
-            password = "guest")
+        conn  <- dbConnect(odbc(),
+                           Driver = "Microsoft Access Driver (*.mdb, *.accdb)", 
+                           DBQ = here("data/trawl_db.accdb"))
         on.exit(dbDisconnect(conn), add = TRUE)
-        dbGetQuery(conn, paste0(
-            "SELECT * FROM City LIMIT ", input$nrows, ";"))
+        tbl(conn, "Specimen") %>% filter(species == 161828) %>% collect()
     })
 }
 
